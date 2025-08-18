9ba304ecb31d3108b0860641f5bde758c1e89c88
elinnnn21-prog <elinnnn21@gmail.com>
2025-08-19 00:02:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-# 앱 실행 확인용
-st.write("앱이 실행 되고 있어요!")
-
-# ========== 기본 설정 ==========
 st.set_page_config(page_title="Pilates Manager", page_icon="🏋️", layout="wide")
+
+# 앱 실행 확인용
+st.title("Pilates Manager")
+st.write("앱이 실행 되고 있어요!")
